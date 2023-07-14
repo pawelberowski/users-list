81f98b88e56c6ede73e2a31af83281ebfba8b174
Taz <pawelberowski@gmail.com>
2023-07-14 17:46:42 +0200

    Added rendering of users names and emails

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import './styles.css';
 
-const paragraph = document.createElement('p');
-paragraph.innerText = 'Hello!';
-document.body.append(paragraph);
+function getUsersData() {
+  return fetch('https://jsonplaceholder.typicode.com/users').then(function (
+    response,
+  ) {
+    return response.json();
+  });
+}
+
+function getUserRow(email, name) {
+  const row = document.createElement('div');
+  const paragraph = document.createElement('p');
+  paragraph.innerText = `${email} ${name}`;
+  row.append(paragraph);
+  return row;
+}
+
+function renderUserData() {
+  const wrapper = document.querySelector('.wrapper');
+  if (!wrapper) {
+    return;
+  }
+  getUsersData().then((users) => {
+    users.forEach(function ({ email, name }) {
+      const row = getUserRow(email, name);
+      wrapper.append(row);
+    });
+  });
+}
+
+renderUserData();
